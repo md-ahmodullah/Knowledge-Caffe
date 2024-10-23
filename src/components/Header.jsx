8bc27625ec42495538bcbc9c5118484d28d0210b
md-ahmodullah <contactme.ahmod@gmail.com>
2024-10-23 08:52:23 +0600

    Added Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="flex items-center justify-between w-10/12 mx-auto my-5">
+      <h1 className="text-4xl font-bold">Knowledge Caffe</h1>
+      <img src="https://i.ibb.co.com/PwFbwKn/profile.png" alt="" />
+    </div>
+  );
+}
